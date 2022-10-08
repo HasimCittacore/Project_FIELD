c5263cdb49b348921283d9e27e80415b54daece0
HasimNewCoder <462mohammed@gmail.com>
2022-10-08 11:13:23 +0530

    first commit

---
+++ force-app/main/default/aura/AccountContactCmp/AccountContactCmpController.js
@@ -0,0 +1,11 @@
+({
+   init: function (cmp, event, helper) {
+        cmp.set('v.mycolumns', [
+                {label: 'Contact Name', fieldName: 'Name', type: 'text'},
+                {label: 'Phone', fieldName: 'Phone', type: 'phone'},
+                {label: 'Email', fieldName: 'Email', type: 'email'},
+                {label: 'Description', fieldName: 'Description', type: 'text'}
+            ]);
+        helper.getData(cmp);
+    }
+})
